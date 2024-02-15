4ae7d59a04560f01b0d3fa11e5d94d380998e106
MrTaiChi <isaaniet@gmail.com>
2024-02-15 15:28:53 -0600

    feat: Termina los metodos de la clase Pila

--- src/main/java/mx/unam/ciencias/edd/Pila.java
+++ src/main/java/mx/unam/ciencias/edd/Pila.java
@@ -10,7 +10,13 @@ public class Pila<T> extends MeteSaca<T> {
      * @return una representación en cadena de la pila.
      */
     @Override public String toString() {
-        // Aquí va su código.
+        Nodo actual = cabeza;
+        String resultado = "";
+        while(actual.siguiente != null){
+            resultado += actual.elemento + "\n";
+            actual = actual.siguiente;
+        }
+        return resultado;
     }
 
     /**
@@ -20,6 +26,17 @@ public class Pila<T> extends MeteSaca<T> {
      *         <code>null</code>.
      */
     @Override public void mete(T elemento) {
-        // Aquí va su código.
+        if(elemento == null){
+            throw new IllegalArgumentException("Elemento invalido");
+        }
+        Nodo nuevo = new Nodo(elemento);
+        if(cabeza == null){
+            cabeza = nuevo;
+            rabo = nuevo;
+            return;
+        }
+        nuevo.siguiente = cabeza;
+        cabeza = nuevo;
+
     }
 }
